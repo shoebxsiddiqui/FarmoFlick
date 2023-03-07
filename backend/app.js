69d92020175f10dee9605eb6dd2cd6c3d0362c29
Mohd Shuaib Siddiqui <shoebsiddiqui4947@gmail.com>
2023-03-07 16:18:13 +0530

    Crated User Logout

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const errorMiddleware = require("./middleware/error");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
 
 // Route Imports
 const product = require("./routes/productRoute");
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -41,3 +41,17 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
 
     sendToken(user, 200, res);
 })
+
+//Logout User
+exports.logout = catchAsyncErrors(async(req, res, next) => {
+
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        message:"Logged Out",
+    });
+});
