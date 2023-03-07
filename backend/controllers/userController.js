d49b466cb0f1d42881f59b0104c057ee5d8cbad4
Mohd Shuaib Siddiqui <shoebsiddiqui4947@gmail.com>
2023-03-07 15:41:56 +0530

    Created Register and Login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
+const sendToken = require("../utils/jwtToken");
 
 // Register a User
 exports.registerUser = catchAsyncErrors( async(req, res, next) =>{
@@ -14,8 +15,29 @@ exports.registerUser = catchAsyncErrors( async(req, res, next) =>{
         },
     });
 
-    res.status(201).json({
-        success: true,
-        user,
-    })
+    sendToken(user, 201, res);
+});
+
+//Login User
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+    const {email, password} = req.body;
+
+    //checking if user has given email and password both
+    if(!email || !password) {
+        return next(new ErrorHandler("Please Enter Email & Password", 400));
+    }
+
+    const user = await User.findOne({email}).select("+password");
+
+    if(!user) {
+        return next(new ErrorHandler("Invalid Enail or Password", 401));
+    }
+
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if(!isPasswordMatched) {
+        return next(new ErrorHandler("Invalid Enail or Password", 401));
+    }
+
+    sendToken(user, 200, res);
 })
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
     name:{
@@ -46,5 +47,16 @@ userSchema.pre("save", async function(next) {
     }
     this.password = await bcrypt.hash(this.password, 10);
 });
+
+//JWT Token
+userSchema.methods.getJWTToken = function() {
+    return jwt.sign({id: this._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE,
+    })
+}
+
+userSchema.methods.comparePassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
 
 module.exports = mongoose.model("User", userSchema);
