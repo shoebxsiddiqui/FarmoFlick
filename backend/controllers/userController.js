884bd08539f58e0ce6bfe89d11aefabc02353885
Mohd Shuaib Siddiqui <shoebsiddiqui4947@gmail.com>
2023-03-24 01:58:48 +0530

    Added getUserDetails, updatePassword and updateProfile feature

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -120,3 +120,55 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
 
     sendToken(user, 200, res);
 });
+
+// Get User Deatils
+exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
+
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+        success: true,
+        user,
+    })
+});
+
+// Update User Password
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+    const user = await User.findById(req.user.id).select("+password");
+
+    const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
+
+    if(!isPasswordMatched) {
+        return next(new ErrorHandler("Old Password is incorrect", 400));
+    }
+
+    if(req.body.newPassword !== req.body.confirmPassword) {
+        return next(new ErrorHandler("Password does not match", 400));
+    }
+
+    user.password = req.body.newPassword;
+
+    await user.save();
+
+    sendToken(user, 200, res);
+});
+
+// Update User Profile
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+    
+    const newUserData = {
+        name: req.body.name,
+        email: req.body.email
+    }
+    //We will add cloudinary later
+    console.log(newUserData);
+    const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
+    console.log(user);
+    res.status(200).json({
+        success: true,
+    });
+});
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { registerUser, loginUser, logout, forgotPassword, resetPassword } = require("../controllers/userController");
+const { registerUser, loginUser, logout, forgotPassword, resetPassword, getUserDetails, updatePassword, updateProfile } = require("../controllers/userController");
+const { isAuthenticatedUser, authorizedRoles } = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/register").post(registerUser);
@@ -11,5 +12,11 @@ router.route("/password/forgot").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 
 router.route("/logout").get(logout);
+
+router.route("/me").get(isAuthenticatedUser, getUserDetails);
+
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
 
 module.exports = router;
