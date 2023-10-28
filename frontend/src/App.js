9610ade793381952705a15c41b738036196d75ec
Mohd Shuaib <shoebsiddiqui4947@gmail.com>
2023-10-28 05:35:58 +0530

    Created React app and made Header and Footer

---
+++ frontend/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import Header from "./component/layout/Header/Header.js";
+import Footer from "./component/layout/Footer/Footer.js";
+import { BrowserRouter as Router } from 'react-router-dom';
+import WebFont from 'webfontloader';
+import React from 'react';
+
+function App() {
+  React.useEffect(() => {
+    WebFont.load({
+      google:{
+        families:["Roboto", "Droid Sans", "Chilanka"]
+      }
+    })
+  }, [])
+  
+  return (
+    <Router>
+      <Header />
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/component/layout/Footer/Footer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import playStore from "../../../images/playstore.png";
+import appStore from "../../../images/Appstore.png";
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+        <div className='leftFooter'>
+            <h4>DOWNLOAD OUR APP</h4>
+            <p>Download App for Android and IOS mobile phones</p>
+            <img src={playStore} alt='playstore' />
+            <img src={appStore} alt='Appstore' />
+        </div>
+        <div className='midFooter'>
+            <h1>ECOMMERCE.</h1>
+            <p>High Quality is our first Priority</p>
+
+            <p>Copyrights 2023 &copy; MeMdShuaib</p>
+        </div>
+        <div className='rightFooter'>
+            <h4>Follow Us</h4>
+            <a href=''>Instagram</a>
+            <a href=''>Youtube</a>
+            <a href=''>Facebook</a>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer;
